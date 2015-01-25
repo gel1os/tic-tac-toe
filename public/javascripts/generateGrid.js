b956babae08d33b642589a8f1c69f42ec231b3d0
kirill T <kirillgel1os@gmail.com>
2015-01-25 17:42:03 +0100

    first commit

---
+++ public/javascripts/generateGrid.js
@@ -0,0 +1,53 @@
+var socket = io(),
+    chosenWeapon;
+
+
+function generateGrid(gridSize) {
+    var arr = Array.apply(null, {length: gridSize}).map(Number.call, Number);
+    $('.wrapper').append($('<div class="grid-container"></div>'));
+    arr.forEach(function (n, i) {
+        $('.grid-container').append($('<div class="row r' + n + '"></div>'));
+        arr.forEach(function (_n) {
+            $('.row.r' + n).append($('<div class="cell c' + _n + ' r' + n + '" data-row="' + n + '" data-cell="' + _n + '"><div class="elem"></div>'));
+        });
+    });
+    return gridSize;
+}
+
+$(document).ready(function () {
+    generateGrid(3);
+});
+
+$(document).on('click', '.cell:not(.filled)', function () {
+        var data = {
+            'data-row': $(this).attr('data-row'),
+            'data-cell': $(this).attr('data-cell'),
+            'weapon': chosenWeapon
+        };
+
+        if (data['weapon']) {
+            socket.emit('fillCell', data);
+        }
+        return false
+});
+
+$(document).on('change', '.weaponType', function () {
+    var weaponType = $(this).attr('value');
+    console.log(weaponType);
+    chosenWeapon = weaponType;
+    socket.emit('chooseWeapon', weaponType);
+});
+
+socket.on('fillCell', function(data){
+    var formData = data,
+        chosenCell = $('.cell.r' + formData['data-row'] + '.c' + formData['data-cell']),
+        elemToFill = chosenCell.find('.elem');
+    chosenCell.addClass('filled');
+    elemToFill.addClass(formData['weapon']);
+});
+
+socket.on('chooseWeapon', function (data) {
+    $('input[id=' + data + ']').attr('disabled', true);
+    $('label[for=' + data + ']').addClass('chosen');
+});
+
